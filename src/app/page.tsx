b96a8908108636d9a5a8d555f0c927d4733d6b57
Arthur Lima <artglima.dev@gmail.com>
2025-01-15 12:25:14 -0300

    feat: creating Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
 import { Header } from "@/components/Header"
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/welcome.svg" alt="Girl coding" />
+      </main>
+    </>
   );
 }
